6980c8ca773028f5c2a5ed9320dc96713d79100d
hyun0213-source <hyun0213@kbu.ac.kr>
2026-09-11 14:18:29 +0900

    2주차 수업 내용

---
+++ week02/src/Main.java
@@ -0,0 +1,10 @@
+
+
+void main() {
+    String name = "김도현";   // 문자열를 데이터를 선언하는 선언문(대입문)
+    System.out.printf("이름 : %s\n", name);
+
+    System.out.print("대한민국\n");
+    System.out.println("경복대학교");
+    System.out.println("김도현");
+}
